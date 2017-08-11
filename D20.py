abd6d051b484c58b2c5dbe30d5c310afaf45a577
JojiG <30815887+JojiG@users.noreply.github.com>
2017-08-11 17:52:03 -0300

    Update D20.py

--- D20.py
+++ D20.py
@@ -1,12 +1,12 @@
 import random
-print('Bilando o dado...')
+print('Rolando o dado...')
 for i in range(1):
     roll = random.randint(1, 20)
     print(roll)
 if roll == 20:
-    print('Eu quero que você se foda e que meu pau cresça! Rolada crítica!')
+    print('Rolada crítica!')
     exit()
 if roll == 1:
-    print('Você broxou! Falha Crítica!')
+    print('Falha Crítica!')
     exit()
     print('Quer tentar de novo?')
